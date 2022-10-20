0af2140f769ec87d2160ca822c16591a9b637ca7
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-10-21 02:22:25 +0700

    feat: indicated if snapshot cannot be found

--- scripts/restore-snapshot.sh
+++ scripts/restore-snapshot.sh
@@ -8,7 +8,7 @@ source "$CURRENT_DIR/variables.sh"
 main() {
   local name="$1"
   local resurrect_restore_script_path="$(get_tmux_option "$resurrect_restore_path_option" "")"
-  if [ -n "$resurrect_restore_script_path" ]; then
+  if [ -n "$resurrect_restore_script_path" ] && [ -n "$name" ]; then
     tmux display-message "Restoring snapshot '$name'..."
     local last_file="$(last_resurrect_file)"
     local original_path="$(last_resurrect_path)"
@@ -19,6 +19,8 @@ main() {
       ln -fs "$last_snapshot" "$last_file"
       "$resurrect_restore_script_path" "quiet" >/dev/null 2>&1
       ln -fs "$original_path" "$last_file"
+    else
+      tmux display-message "Snapshot '$name' not found"
     fi
   fi
 }
